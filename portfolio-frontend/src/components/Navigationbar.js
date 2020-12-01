1d7545ec93baa6f309dc9319c0d7377cf75b0c69
Cristian Tavarez <cristian.javier.11@hotmail.com>
2020-12-01 14:44:59 -0400

    favicon & responsive taskbar

--- portfolio-frontend/src/components/Navigationbar.js
+++ portfolio-frontend/src/components/Navigationbar.js
@@ -1,17 +1,19 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 const Navigationbar = () => {
   return (
-    <Navbar sticky="top" bg="main" expand="lg" collapseOnSelect className="d-flex" variant="dark">
-      <Navbar.Brand href="#home">Portfolio</Navbar.Brand>
-      <Navbar.Toggle className="ml-auto" aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
-        <Nav>
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Contact</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+    <Navbar sticky="top" bg="dark" variant="dark" expand="lg" collapseOnSelect className="d-flex">
+      <Container>
+        <Navbar.Brand href="#home">Cristian Tavarez</Navbar.Brand>
+        <Navbar.Toggle className="ml-auto" aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+          <Nav>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
