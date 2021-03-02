ac7c51af4749316eeee9529a8da517d9ae79bcdd
Cristian Tavarez <cristian.javier.11@hotmail.com>
2021-03-02 18:19:04 -0400

    footer links added

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -9,6 +9,8 @@ const Footer = () => {
         <Container className="justify-content-center">
           <Nav>
             <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav>
         </Container>
