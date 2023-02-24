40d2ad243e91001ad3324ca0060388633655c4e5
Cristian Tavarez <cristian.javier.11@hotmail.com>
2023-02-24 19:32:21 -0400

    Fix href 2

--- frontend/src/components/ContactForm.js
+++ frontend/src/components/ContactForm.js
@@ -137,7 +137,7 @@ const ContactForm = () => {
                   <article className="mb-5">
                     <Card className="shadow-sm">
                       <Card.Body className="d-flex flex-column justify-content-center text-center">
-                        <a>
+                        <a href="https://crisja04.me/">
                           <Card.Title>
                             <FaPhone />
                           </Card.Title>
@@ -151,7 +151,7 @@ const ContactForm = () => {
                   <article className="mb-5">
                     <Card className="shadow-sm">
                       <Card.Body className="d-flex flex-column justify-content-center text-center">
-                        <a>
+                        <a href="https://crisja04.me/">
                           <Card.Title>
                             <FaWhatsapp />
                           </Card.Title>
